5c36db98bd0ff2157df20936979348ff19c36d4d
Pierre Dias <cdias.pierre@gmail.com>
2023-01-02 20:54:00 -0300

    Estrutura Inicial Parte 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,21 @@
+import * as C from "./App.styles";
+import logoImage from "./assets/devmemory_logo.png";
+
 const App = () => {
-  return (<div>...</div>)
-}
+  return (
+    <C.Container>
+      <C.Info>
+        <C.LogoLink href="">
+          <img src={logoImage} alt="" width="200" />
+        </C.LogoLink>
+
+        <C.InfoArea>...</C.InfoArea>
+        <button>Reiniciar</button>
+      </C.Info>
+
+      <C.GridArea>...</C.GridArea>
+    </C.Container>
+  );
+};
 
 export default App;
