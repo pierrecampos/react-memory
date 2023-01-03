e23647d0e45a532dc05d31b57ddc68a99a3a9a35
Pierre Dias <cdias.pierre@gmail.com>
2023-01-03 19:55:45 -0300

    Componente Botão

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import * as C from "./App.styles";
 import logoImage from "./assets/devmemory_logo.png";
+import restartIcon from "./svgs/restart.svg";
 import { InfoItem } from "./components/InfoItem";
+import { Button } from "./components/Button";
 
 const App = () => {
+  const resetAndCreateGrid = () => {};
+
   return (
     <C.Container>
       <C.Info>
@@ -14,7 +18,11 @@ const App = () => {
           <InfoItem label="Tempo" value="00:00"></InfoItem>
           <InfoItem label="Movimentos" value="0"></InfoItem>
         </C.InfoArea>
-        <button>Reiniciar</button>
+        <Button
+          label="Reiniciar"
+          icon={restartIcon}
+          onClick={resetAndCreateGrid}
+        ></Button>
       </C.Info>
 
       <C.GridArea>...</C.GridArea>
